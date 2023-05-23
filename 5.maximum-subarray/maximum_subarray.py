2f6cb3808d57ad5512a8d28761216a59235522ec
Noman-Tanveer <nomantanveer021@gmail.com>
2023-05-23 18:52:17 +0500

    Added maximum subarray:TBI

---
+++ 5.maximum-subarray/maximum_subarray.py
@@ -0,0 +1,11 @@
+# Brute-force solution
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sums = []
+        for i in range(len(nums) + 1):
+            for j in range(i):
+                sums.append(sum(nums[j: i]))
+        return max(sums)
+
+# improved solution
