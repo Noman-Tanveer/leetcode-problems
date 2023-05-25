8b1d597f6b50c31a7a739075a02dfa4b693b1f59
Noman-Tanveer <nomantanveer021@gmail.com>
2023-05-25 15:26:46 +0500

    Added improved solutions P::3,4

--- 4.product-except-self/product_except_self.py
+++ 4.product-except-self/product_except_self.py
@@ -1,5 +1,7 @@
 import math
 import copy
+from typing import List
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         arr = []
@@ -8,6 +10,30 @@ class Solution:
             numbers.remove(i)
             arr.append(math.prod(numbers))
         return arr
-        
-# Improved solution
 
+# Improved solution
+import math
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre_prod = [1]*(len(nums))
+        post_prod = [1]*(len(nums))
+        prod = [1]*(len(nums))
+        leftp = 1
+        rightp = 1
+
+        for i in range(len(nums)):
+            pre_prod[i] = leftp
+            leftp *= nums[i]
+
+        for j in range(len(nums)-1, -1, -1):
+            post_prod[j] = rightp
+            rightp *= nums[j]
+
+        prod[0] = post_prod[0]
+        prod[len(nums)-1] = pre_prod[len(nums)-1]
+
+        for i in range(1, len(nums)-1):
+            prod[i] = pre_prod[i]*post_prod[i]
+
+        return prod
