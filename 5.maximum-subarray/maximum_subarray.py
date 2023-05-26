f3c5e491a784e878a394dbf6c487ba12f255a603
Noman-Tanveer <nomantanveer021@gmail.com>
2023-05-26 14:41:43 +0500

    Added improved solution for P5

--- 5.maximum-subarray/maximum_subarray.py
+++ 5.maximum-subarray/maximum_subarray.py
@@ -10,3 +10,15 @@ class Solution:
         return max(sums)
 
 # improved solution
+# Dynamic programming
+class Solution:
+    def maxSubArray(self, nums):
+        @cache
+        def solve(i, must_pick):
+            if i >= len(nums):
+                if must_pick:
+                    return 0
+                else:
+                    return -inf
+            return max(nums[i] + solve(i+1, True), 0 if must_pick else solve(i+1, False))
+        return solve(0, False)
