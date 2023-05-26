01d4cfb760d4300d819c72f8f25757d37514946b
Noman-Tanveer <nomantanveer021@gmail.com>
2023-05-26 14:52:31 +0500

    Added improved solution for P7

--- 7.minimum-in-rotated-sorted-array/min_in_rotated_array.py
+++ 7.minimum-in-rotated-sorted-array/min_in_rotated_array.py
@@ -6,3 +6,15 @@ class Solution:
         return min(nums)
 
 # Improved solution
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums)-1
+
+        while left < right:
+            mid = left + int((right - left) / 2)
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
