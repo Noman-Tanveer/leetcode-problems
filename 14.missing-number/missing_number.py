134836ed949c72ee3005545a400adec673581251
Noman-Tanveer <nomantanveer021@gmail.com>
2023-05-31 11:37:44 +0500

    Added solution for P14: TBI

---
+++ 14.missing-number/missing_number.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if i in nums:
+                continue
+            else:
+                return i
+        return len(nums)
