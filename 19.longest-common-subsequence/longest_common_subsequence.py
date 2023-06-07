35a03356f17872b416b42e5341bedac2352cd3a7
Noman-Tanveer <nomantanveer021@gmail.com>
2023-06-07 19:06:23 +0500

    Added optmial solution

--- 19.longest-common-subsequence/longest_common_subsequence.py
+++ 19.longest-common-subsequence/longest_common_subsequence.py
@@ -1,4 +1,5 @@
 # Incorrect solution does take into account the order of letters in strings
+from typing import List
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
@@ -11,3 +12,24 @@ class Solution:
                     lens[j][i] = max(lens[j-1][i], lens[j][i-1])
         return lens[-1][-1]
 
+# Improed solution
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def construct(current,wordDict, memo={}):
+            if current in memo:
+                return memo[current]
+
+            if not current:
+                return True
+
+            for word in wordDict:
+                if current.startswith(word):
+                    new_current = current[len(word):]
+                    if construct(new_current,wordDict,memo):
+                        memo[current] = True
+                        return True
+
+            memo[current] = False
+            return False
+
+        return construct(s,wordDict)
