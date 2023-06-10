612753e8a0490fa99be95879b98a1ccf35f1a958
Noman-Tanveer <nomantanveer021@gmail.com>
2023-06-10 12:11:11 +0500

    Added solution to P22

---
+++ 22.house-robber/house_robber.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = [0] * len(nums)
+        cache[0] = nums[0]
+        if len(nums) == 1:
+            return cache[0]
+        cache[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            cache[i] = max(cache[i-1], cache[i-2]+nums[i])
+        return max(cache[-1], cache[-2])
