4ac14c2a52aa37ec0060bcf0e3c36e8dc9edb6bc
Noman-Tanveer <nomantanveer021@gmail.com>
2023-06-14 14:20:18 +0500

    Added solution to problem 24

---
+++ 24.decode-ways/decode_ways.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        n = len(s)
+        cache = [0]* (n+1)
+        # base case
+        cache[0] = 1
+        for i in range(1, n + 1):
+            if s[i - 1] != '0':
+                cache[i] = cache[i - 1]
+            if i >= 2:
+                x = int(s[i - 2: i])
+                if 10 <= x <= 26:
+                    cache[i] += cache[i - 2]
+        return cache[n]
