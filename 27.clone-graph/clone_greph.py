4c897d9cb5b00a4f644fa1c2502f91b04160ad3a
Noman-Tanveer <nomantanveer021@gmail.com>
2023-06-16 11:53:11 +0500

    Added improved BFS solution to P27

--- 27.clone-graph/clone_greph.py
+++ 27.clone-graph/clone_greph.py
@@ -12,3 +12,30 @@ class Solution:
 
 
 # Improved solution
+
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+import copy
+from collections import deque
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+
+        q = deque([node])
+        clones = {node.val: Node(node.val, [])}
+
+        while q:
+            curr = q.popleft()
+            curr_clones = clones[curr.val]
+            for ngbr in curr.neighbors:
+                if ngbr.val not in clones:
+                    clones[ngbr.val] = Node(ngbr.val, [])
+                    q.append(ngbr)
+
+                curr_clones.neighbors.append(clones[ngbr.val])
+
+        return clones[node.val]
