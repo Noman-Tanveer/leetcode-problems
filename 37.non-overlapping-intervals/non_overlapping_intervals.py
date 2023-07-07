7af13e41f3863b2e077a20b27e4b7c51d2945cd3
Noman-Tanveer <nomantanveer021@gmail.com>
2023-07-07 11:21:41 +0500

    Added solution to the non-overlapping interval problem

---
+++ 37.non-overlapping-intervals/non_overlapping_intervals.py
@@ -0,0 +1,14 @@
+# Simple solution
+from typing import List
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # Needs to be done with dynamic programming
+        std_itvls = sorted(intervals, key=lambda x: x[1]*10+x[0])
+        removed = 0
+        selected = [std_itvls[0]]
+        for i in std_itvls[1:]:
+            if i[0] >= selected[-1][-1]:
+                selected.append(i)
+            else:
+                removed += 1
+        return removed
